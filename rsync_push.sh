efce87b8d23f0e69ef3845855437dcead1d4ff19
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-02-02 11:35:49 +0100

    Updata

--- rsync_push.sh
+++ rsync_push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#rsync -aP ./dest/ srv1:/srv/www/zhr.niedzwiedzinski.cyou/harcdzielnia
-rsync -aP ./dest/ srv3:/srv/www/harcdzielnia.19pdh.pl
+rsync -aP ./dest/ srv3:/srv/www/zhr.niedzwiedzinski.cyou/harcdzielnia
+#rsync -aP ./dest/ srv3:/srv/www/harcdzielnia.19pdh.pl
